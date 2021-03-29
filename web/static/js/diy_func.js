fb3c022617fcbeb545915b8d3759fd5313d36334
Ji-yong219 <76960714+Ji-yong219@users.noreply.github.com>
2021-03-30 04:02:17 +0900

    result_canvas_ui

--- web/static/js/diy_func.js
+++ web/static/js/diy_func.js
@@ -5,6 +5,7 @@ $(window).on('load', function(){
     
     var brush_size = 8;
 
+    // 슬라이드 조절 시 브러시 사이즈 변경
     $('#brush_size').change(function(e){
         var slider = document.getElementById('brush_size');
         brush_size = $(this).val();
@@ -13,14 +14,13 @@ $(window).on('load', function(){
 
 
 
-
     function make_base(path) {
         image = new Image();
         image.src = path;
 
-
         $(image).on('load', function(){
             var pic_canvas = document.getElementById('canvas_pic');
+            var result_canvas = document.getElementById('canvas_result');
 
             var width_set = 600;
             var height_set = 600 * image.height / image.width;
@@ -31,7 +31,13 @@ $(window).on('load', function(){
             pic_canvas.style.width = width_set;
             pic_canvas.style.height = height_set;
 
+            result_canvas.width = width_set;
+            result_canvas.height = height_set;
+
+
             var ctx = pic_canvas.getContext('2d');
+            var result_ctx = result_canvas.getContext('2d');
+
             ctx.fillStyle = "rgba(255, 0, 0, 0.05)";
             
             // ctx.globalAlpha = "0.5";
@@ -43,18 +49,14 @@ $(window).on('load', function(){
             //  이미지의 왼위 부분x, 이미지의 왼위 부분y, 이미지의 원하는 가로크기, 
             //  이미지의 원하는 세로크기,
             //  사각형 부분x, 사각형 부분y, 가로크기, 세로크기)
-            ctx.drawImage(image, 0, 0, width_set, height_set );
+            ctx.drawImage( image, 0, 0, width_set, height_set );
 
             $("#original_img_url").text(path);
-            $(".canvas_box, .convert_box").css('height', height_set + 70);
+            $(".canvas_box, .convert_box").css('height', height_set + 90);
 
-            
-            
             
             var isDrawing, lastPoint;
               
-            var result_canvas = document.getElementById('canvas_result');
-            var result_ctx = result_canvas.getContext('2d');
 
             
             pic_canvas.onmousedown = function(e) {
@@ -63,7 +65,9 @@ $(window).on('load', function(){
             };
             
             pic_canvas.onmousemove = function(e) {
-                if (!isDrawing) return;
+                if (!isDrawing){
+                    return;
+                }
                 
                 var currentPoint = { x: e.offsetX, y: e.offsetY }; // 왼쪽 위
 
@@ -82,7 +86,6 @@ $(window).on('load', function(){
                     ctx.arc(x + x_value, y + y_value, brush_size, false, Math.PI * 2, false);
                     ctx.closePath();
                     ctx.fill();
-                    // ctx.stroke(); // 이거 하면 border생김
                 }
                 
                 lastPoint = currentPoint;
@@ -98,8 +101,9 @@ $(window).on('load', function(){
             function clearit() {
                 ctx.clearRect(0,0, 1000, 1000);
                 make_base(image_path);
+                brush_size = 8;
             }
-            $("#clear_btn").click(function(){
+            $("#pic_clear_btn").click(function(){
                 clearit();
             });
         });
@@ -107,20 +111,17 @@ $(window).on('load', function(){
         
     }
 
-    function sketchit(e) {
-        context.beginPath()
-        context.moveTo(mouseX, mouseY);
-        context.lineTo(mouseX,mouseY)
-        context.lineCap = 'round';
-        context.lineWidth = document.getElementById('brush_size').value;
-        context.strokeStyle = document.getElementById('color').value;
-        context.stroke();
-        MouseIsDown = true;
-    };
+    // function sketchit(e) {
+    //     context.beginPath()
+    //     context.moveTo(mouseX, mouseY);
+    //     context.lineTo(mouseX,mouseY)
+    //     context.lineCap = 'round';
+    //     context.lineWidth = document.getElementById('brush_size').value;
+    //     context.strokeStyle = document.getElementById('color').value;
+    //     context.stroke();
+    //     MouseIsDown = true;
+    // };
 });
-
-
-
 
 
 
@@ -131,15 +132,3 @@ function distanceBetween(point1, point2) {
 function angleBetween(point1, point2) {
     return Math.atan2( point2.x - point1.x, point2.y - point1.y );
 }
-/*
-// 캔버스 아래 이미지 깔기
-function image_onCanvas(){
-    var image = new Image();
-
-    image.addEventListener('load', function(){
-        var ctx = document.getElementById("canvas_pic").getContext("2d");
-        ctx.drawImage(image, 0, 0,0 200, 200);
-    }, false);
-
-    image.src = "test-image/sunf.jpg";
-}*/
