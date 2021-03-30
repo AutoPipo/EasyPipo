984223814be8c532565d84b37f4fc6a6aa514405
구민구 <koomk97@naver.com>
2021-03-31 02:39:26 +0900

    Create getSession.py

---
+++ libs/getSession.py
@@ -0,0 +1,14 @@
+﻿
+import random, datetime, os
+
+class Sessions:
+    def __init__(self):
+        self.__session_id = self.__makeSession()
+        
+    def __makeSession(self):
+        nowTime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+        session_id = nowTime + str(random.randint(11 , 999999))
+        return session_id
+        
+    def getSession(self):
+        return self.__session_id
