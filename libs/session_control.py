c73e954b759499af977130bb76f1c0a0aa28e8ca
구민구 <koomk97@naver.com>
2021-03-31 02:58:19 +0900

    session

--- libs/session_control.py
+++ libs/session_control.py
@@ -1,14 +1,14 @@
 ﻿
 import random, datetime, os
 
-class Sessions:
+class sessionControl:
     def __init__(self):
         self.__session_id = self.__makeSession()
         
-    def __makeSession(self):
+    def __createSession(self):
         nowTime = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
         session_id = nowTime + str(random.randint(11 , 999999))
         return session_id
         
-    def getSession(self):
+    def get(self):
         return self.__session_id
