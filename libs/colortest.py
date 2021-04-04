641d28e1c55e22e9d14ac8ea0c68fb2c7f4dd475
구민구 <koomk97@naver.com>
2021-04-05 02:54:18 +0900

    painting test

--- libs/colortest.py
+++ libs/colortest.py
@@ -3,7 +3,7 @@
 
 import time, os
 from painting import Painting
-from drawLine import DrawLine
+from drawLine import DrawLine, imageExpand
 import cv2
 
 def imageSave(image, directory = "./result-image/", name = "", id=""):
@@ -15,34 +15,49 @@ def imageSave(image, directory = "./result-image/", name = "", id=""):
 start = time.time()
 
 dir = "./test-image/"
-file = "her1"
+file = "lala"
 base = ".jpg"
-id = "nosiminal"
+id = "0"
 
 painting = Painting( dir+file+base )
-blurImage = painting.blurring(div = 32, radius = 10, sigmaColor = 30, medianValue=3)
+'''
+# org
+blurImage = painting.blurring(div = 32, radius = 10, sigmaColor = 30, medianValue=5)
 imageSave(blurImage, name = file+"-blur", id=id)
 
+similarMap = painting.getSimilarColorMap(blurImage, value = 10, direction = "h" )
+imageSave(similarMap, name = file+"-similar", id=id)
+'''
 
-# similarMap = painting.getSimilarColorMap(blurImage, value = 15, direction = "h" )
-# imageSave(similarMap, name = file+"-similar", id=id)
-# paintingMap = painting.getPaintingColorMap(similarMap)
+# test
+similarMap = painting.getSimilarColorMap( value = 12, direction = "h" )
+imageSave(similarMap, name = file+"-similar", id=id)
+print("========  Similar Map End  =======")
+blurImage = painting.blurring(similarMap, div = 50, radius = 10, sigmaColor = 30, medianValue=7)
+imageSave(blurImage, name = file+"-blur", id=id)
+print("========  Blur Map End  =======")
+# test finish
 
 paintingMap = painting.getPaintingColorMap(blurImage)
+# paintingMap = painting.getPaintingColorMap(blurImage)
+
 imageSave(paintingMap, name = file+"-painting", id=id)
 
 
-colorDict = painting.getColorDict(paintingMap)
+# colorDict = painting.getColorDict(paintingMap)
 print("=="*20)
-print("COLOR NUMBER : ", len(colorDict))
+# print("COLOR NUMBER : ", len(colorDict))
 print("=="*20)
 
-drawLine = DrawLine(paintingMap, mergeValue = 1)
-lineMap = drawLine.lineMap
-# lineOnImage = drawLine.getLineOnImage()
-expandImage = drawLine.imageExpand(lineMap, guessSize = True)
+drawLine = DrawLine(paintingMap)
+lineMap = drawLine.getDrawLine(value = 1)
+imageSave(lineMap, name = file+"-line", id=id)
 
+expandImage = imageExpand(lineMap, guessSize = True)
 imageSave(expandImage, name = file+"-expand", id=id)
+
+print("========= Expand Process ========")
+
 
 print("time :", round((time.time() - start)/60, 3) ,"분 정도.." )
 
