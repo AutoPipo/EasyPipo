88e8e80dd817dc4b7890fe9953a611c403ab0d1e
Ji-yong219 <wldydslapjyy@naver.com>
2022-02-28 18:51:36 +0900

    update host

--- __main__.py
+++ __main__.py
@@ -3,8 +3,8 @@ from web.app import create_app
 
 app = create_app()
 app.run( 
-    host = "0.0.0.0",
-    port = 5002,
+    host = "localhost",
+    port = 5000,
     debug = False,
     use_reloader=False
 )
