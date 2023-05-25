f06810754f3f84533ce0af648f46a33808edb020
Minku Koo <koomk97@naver.com>
2023-05-25 23:14:38 +0900

    to avoid asscalar error
    Temporary to avoid numpy asscalar error from colormath

--- __main__.py
+++ __main__.py
@@ -1,5 +1,11 @@
 ﻿# -*- coding: utf-8 -*-
 from web.app import create_app
 
+# Temporary to avoid No Attribute'asscalar' Error
+import numpy
+def patch_asscalar(a):
+    return a.item()
+setattr(numpy, "asscalar", patch_asscalar)
+
 app = create_app()
 app.run()
