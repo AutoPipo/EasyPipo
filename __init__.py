30bc88cdfca99faa8190b6889ee8dae3cab93629
Ji-yong219 <wldydslapjyy@naver.com>
2023-06-22 21:26:15 +0900

    style: add comment to numpy avoid code

--- __init__.py
+++ __init__.py
@@ -1,6 +1,13 @@
 # 2023 EasyPipo
 
-# Temporary to avoid No Attribute'asscalar' Error
+"""Temporary to avoid No Attribute'asscalar' Error
+File: __init__.py
+Created: 2023-06-22
+
+@author: Minku-Koo
+LastModifyDate: 2023-06-22 
+LastModifier: Ji-yong219
+"""
 import numpy
 def patch_asscalar(a):
     return a.item()
