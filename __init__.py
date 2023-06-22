a4f6193c977471da31c3cff07bf620b90f459192
Ji-yong219 <wldydslapjyy@naver.com>
2023-06-22 21:11:00 +0900

    refactor: move numpy avoid code

--- __init__.py
+++ __init__.py
@@ -1,1 +1,7 @@
 # 2023 EasyPipo
+
+# Temporary to avoid No Attribute'asscalar' Error
+import numpy
+def patch_asscalar(a):
+    return a.item()
+setattr(numpy, "asscalar", patch_asscalar)
--- __main__.py
+++ __main__.py
@@ -1,11 +1,6 @@
 ﻿# -*- coding: utf-8 -*-
 from web.app import create_app
 
-# Temporary to avoid No Attribute'asscalar' Error
-import numpy
-def patch_asscalar(a):
-    return a.item()
-setattr(numpy, "asscalar", patch_asscalar)
-
-app = create_app()
-app.run()
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
